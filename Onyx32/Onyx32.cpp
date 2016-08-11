2c3ef09ee5923b5bb9617e4633688b4917c5b08e
Vasilios Magriplis <vasilios.magriplis@radicalsystems.com.au>
2016-08-11 22:51:03 +0930

    Minor cleanup.

--- Onyx32/Onyx32.cpp
+++ Onyx32/Onyx32.cpp
@@ -1,6 +1,3 @@
-// Onyx32.cpp : Defines the entry point for the application.
-//
-
 #include "stdafx.h"
 #include "Onyx32.h"
 #include "../Onyx.Gui/include/Onyx.Gui.h"
@@ -9,42 +6,10 @@
 
 using namespace Onyx::Windowing;
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR    lpCmdLine, _In_ int       nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // TODO: Place code here.
-	
-    // Initialize global strings
-    //LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    //LoadStringW(hInstance, IDC_ONYX32, szWindowClass, MAX_LOADSTRING);
-    //MyRegisterClass(hInstance);
-
-    //// Perform application initialization:
-    //if (!InitInstance (hInstance, nCmdShow))
-    //{
-    //    return FALSE;
-    //}
-
-    //HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_ONYX32));
-
-    //MSG msg;
-
-    //// Main message loop:
-    //while (GetMessage(&msg, nullptr, 0, 0))
-    //{
-    //    if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-    //    {
-    //        TranslateMessage(&msg);
-    //        DispatchMessage(&msg);
-    //    }
-    //}
-
-    //return (int) msg.wParam;
 
 	IWindow* wnd = Factory::TestWindowing();
 	wnd->Initialize();
