5ba6894e5b7575713b6211cee9b84f6e86f6f90a
Vasilios Magriplis <etameson@gmail.com>
2019-06-12 00:35:32 +0930

    Clean up Factory.cpp

--- src/gui/Onyx32.Gui/src/cpp/Factory.cpp
+++ src/gui/Onyx32.Gui/src/cpp/Factory.cpp
@@ -1,10 +1,5 @@
 #include "../h/stdafx.h"
-#include "../../include/Onyx32.Gui.h"
-#include "../Window/Window.h"
-#include "../WindowClassManager/WindowClassManager.h"
 #include "../FormBuilder/FormBuilder.h"
-#include "../h/Resource.h"
-#include "../h/dllmain.h"
 
 namespace Onyx32::Gui
 {
